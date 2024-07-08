6538958ea101fc174144b8dfa64f57ca2557aa24
ryanglatour <ryanglatour@gmail.com>
2024-07-08 16:59:49 -0400

    Initialize backend

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+const path = require('path')
+require("dotenv").config({ path: path.resolve(__dirname, '../.env'), override: true})
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use((req, res, next) => 
+{
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PATCH, DELETE, OPTIONS'
+  );
+  next();
+});
+
+
+app.listen(process.env.PORT); // start Node + Express server on port 5000
+console.log("listening on " + process.env.PORT)
